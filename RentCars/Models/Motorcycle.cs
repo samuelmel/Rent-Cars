6134e963555d538d8790786609875bd019c1288e
Samuel Mendes <157438445+samuelmel@users.noreply.github.com>
2025-05-14 15:10:30 -0300

    Update Motorcycle.cs

--- RentCars/Models/Motorcycle.cs
+++ RentCars/Models/Motorcycle.cs
@@ -2,7 +2,6 @@ using RentCars.Types;
 
 namespace RentCars.Models;
 
-//7 - Faça a classe `Motorcycle` herdar de `Vehicle`
 public class Motorcycle : Vehicle
 {
     public double SeatHeight{ get; set;}
